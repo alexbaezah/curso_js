9b12591f93e016a0e10278cbc4abbdcdc232ef76
alex b <alcebaeza@gmail.com>
2022-10-11 00:08:18 -0300

    agregado en el dom para crear todo list con js y se imprima en el html

--- dom/main_2.js
+++ dom/main_2.js
@@ -1,17 +1,27 @@
+// guardaremos todo dentro de un arreglo
+
+
 window.onload = () => {
+    const todos = [];
     const form = document.getElementById('todo-form');
     form.onsubmit = (e) => {
         // e.prventDefatult() previene que se refresque la pantalla
         e.preventDefault();
         // luego vamos la campo de todo
-        const todo = document.getElementById('todo')
+        const todo = document.getElementById('todo');
         //guardamos el valor en un variable
         //recordar que es un value y no un innerText
         const todoText = todo.value;
         //se reemplaza el valor por un valor vacío, 
         // con el fin de que el input quede vacío o "refrescado"
         todo.value = '';
-        console.log(todoText);
+        todos.push(todoText)
+        //console.log(todoText);
+        const todoList = document.getElementById('todo-list');
+        todoList.innerHTML = '';
+        for(let i = 0; i < todos.length; i++){
+            todoList.innerHTML += '<li>' + todos[i] + '</li>';
+        }
     }
 
 }
